2a87a79abb84b8fe1beb7994d1a89f7464632767
susankoju <aksusankoju@gmail.com>
2020-01-27 05:08:48 +0545

    Updates

---
+++ precache-manifest.151dd02f5903964a15463640215510d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67dc548778b1825c11463ab1603f647",
+    "url": "/react-messageall/index.html"
+  },
+  {
+    "revision": "111f88afc675e8b57f1d",
+    "url": "/react-messageall/static/css/main.e5419d69.chunk.css"
+  },
+  {
+    "revision": "174e0a6dd4e85e37b5c7",
+    "url": "/react-messageall/static/js/2.52ea343f.chunk.js"
+  },
+  {
+    "revision": "138a6d51240ab070e8d1b20c54be6394",
+    "url": "/react-messageall/static/js/2.52ea343f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "111f88afc675e8b57f1d",
+    "url": "/react-messageall/static/js/main.cc4d19ed.chunk.js"
+  },
+  {
+    "revision": "b81fc85e2f85811b1e8a",
+    "url": "/react-messageall/static/js/runtime-main.11f0ac5d.js"
+  }
+]);
